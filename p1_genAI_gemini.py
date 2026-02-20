8bfd55d6aa41ea16d59d64e01893905bf6675fa9
Alok Singh <44195690+aalokvsingh@users.noreply.github.com>
2026-02-20 09:19:46 +0000

    added code

--- p1_genAI_gemini.py
+++ p1_genAI_gemini.py
@@ -1,16 +1,23 @@
 import os
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openrouter import ChatOpenRouter
 from langchain_core.messages import HumanMessage
 
 # Load environment variables
 load_dotenv()
 
 # Initialize Gemini model (cheaper testing model)
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
-    temperature=0.3,
+# llm = ChatGoogleGenerativeAI(
+#     model="gemini-2.5-flash",
+#     google_api_key=os.getenv("GOOGLE_API_KEY"),
+#     temperature=0.3,
+# )
+
+# It automatically looks for the OPENROUTER_API_KEY environment variable
+llm = ChatOpenRouter(
+    model="google/gemini-2.5-flash",
+    temperature=0.7,
 )
 
 def main():
